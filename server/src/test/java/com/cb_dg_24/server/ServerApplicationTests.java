e5acc3ab2d7cc7cd570fc01e29338155e49df072
Danny Orton <52336508+dannyvorton@users.noreply.github.com>
2025-06-09 16:06:10 -0600

    initial commit

---
+++ server/src/test/java/com/cb_dg_24/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb_dg_24.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
