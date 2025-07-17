639b1420c6b8cbd0fb06afba713e4ae1d607f31b
Danny Orton <52336508+dannyvorton@users.noreply.github.com>
2025-07-17 15:45:20 -0600

    update stuff

--- browser/src/components/List.vue
+++ browser/src/components/List.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+/*
+  import PocketBase from 'pocketbase';
+
+  const pb = new PocketBase('http://127.0.0.1:8090');
+
+  const records = await pb.collection('posts').getFullList({
+    sort: '-created',
+  });
+*/
+</script>
+
 <template>
   <h1>Hello World!</h1>
 </template>
