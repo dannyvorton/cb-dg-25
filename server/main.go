3c4382ec78d1e5aec15f8656af1e3f748b958d55
Danny Orton <52336508+dannyvorton@users.noreply.github.com>
2025-07-17 09:58:46 -0600

    update main

--- server/main.go
+++ server/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
 	app := pocketbase.New()
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
-		se.Router.GET("/hello", func(re *core.RequestEvent) error {
-			return re.String(200, "Hello world!")
-		})
+		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 		return se.Next()
 	})
 	if err := app.Start(); err != nil {
