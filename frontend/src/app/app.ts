19ddc54f984f0261752c8ba96b649860e799bc47
Danny Orton <52336508+dannyvorton@users.noreply.github.com>
2025-09-15 16:45:36 -0600

    add components

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { List } from './list/list';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, List, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
